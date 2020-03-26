f3827b08b55633f28204c93291b014b385f46c5b
2378694b <60888106+2378694b@users.noreply.github.com>
2020-03-26 19:30:30 +0000

    Add files via upload

--- MoneyFish/settings.py
+++ MoneyFish/settings.py
@@ -15,7 +15,9 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
@@ -37,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'moneyfishapp'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +57,7 @@ ROOT_URLCONF = 'MoneyFish.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR, ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -99,6 +102,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+STATICFILES_DIRS = [STATIC_DIR, ]
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
--- MoneyFish/urls.py
+++ MoneyFish/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from moneyfishapp import views
 
 urlpatterns = [
+    path('', views.index, name= 'index'),
+    path('moneyfishapp/', include('moneyfishapp.urls')),
     path('admin/', admin.site.urls),
 ]
